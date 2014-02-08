b284cebc71237d8c395a7c4dd3dcde86bce2ed8f
Geraint Harries <geraint@geraintharries.co.uk>
2014-02-08 15:57:04 +0000

    Finished Encoding Function

---
+++ DynamicDecoding.py
@@ -0,0 +1,9 @@
+file = 'decodedFile.txt'
+
+with open(file) as f:
+	for i in range(0, 20):
+    		print f.read(4)
+
+# read dictionary
+
+# Decode file
--- dynamicEncoding.py
+++ dynamicEncoding.py
@@ -6,9 +6,10 @@ chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@$()_+-=
 
 total = 0
 acumulator = 0
-
-
 dictionary = []
+i = 0
+tf = True
+j = 0
 
 for char in chars:
         count = text.count(char)
@@ -18,13 +19,6 @@ for char in chars:
                 dictionary.insert(acumulator, char)
         acumulator += 1
 
-i = 0
-
-tf = True
-j = 0
-
-#ADD HUFFMAN CODE
-
 while tf == True:
         if 2**j >= total:
                 print "Encode with %i bits" % (j)
@@ -33,13 +27,36 @@ while tf == True:
 
 encodingDictionary = open('encodingDictionary.txt','w')
 
+fullDictionary = {}
+
 while i < len(dictionary):
+
 	binary = bin(i).replace('0b', '')
-	binaryFormat = "{0:0>4}".format(binary)
+	string = '{0:0>%s}' % (j-1)
+	binaryFormat = string.format(binary)
 
 	encodingDictionary.write("%s, %s; " % (binaryFormat, dictionary[i]))
 	print "%s, \t %s" % (binaryFormat, dictionary[i])
+	fullDictionary[dictionary[i]] = binaryFormat
 	i += 1
+
+print fullDictionary
+
+decodedFile = open('decodedFile.txt', 'w')
+
+with open('file.txt') as f:
+  while True:
+    c = f.read(1)
+    if not c:
+      print "End of file"
+      break
+    decodedFile.write(fullDictionary[c])
+
+
+
+
+
+
 
 
 
